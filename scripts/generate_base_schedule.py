9e478a612ffb1c0cc796c365fabbaf738027cba6
Yaniv Yacoby <yanivyacoby@gmail.com>
2025-10-05 12:53:28 -0400

    updated

--- scripts/generate_base_schedule.py
+++ scripts/generate_base_schedule.py
@@ -7,19 +7,23 @@ DISPLAY_DAYS = CLASS_DAYS
 
 
 SPECIAL_DATES = [
-    (datetime.datetime(2025, 9, 1), 'Labor Day: no classes.'),
-    (datetime.datetime(2025, 10, 13), 'Indigenous Peoples\' Day: no classes.'),
-    (datetime.datetime(2025, 10, 14), 'Fall Break: no classes.'),
-    (datetime.datetime(2025, 10, 28), 'Tanner Conference: no classes.'),
-    (datetime.datetime(2025, 11, 26), 'Thanksgiving Break: no classes.'),    
-    (datetime.datetime(2025, 11, 27), 'Thanksgiving Break: no classes.'),    
-    (datetime.datetime(2025, 11, 28), 'Thanksgiving Break: no classes.'),    
-    (datetime.datetime(2025, 12, 11), 'Reading Period Begins.'),    
-    (datetime.datetime(2025, 12, 15), 'Final Exam Period Begins.'),    
+    (datetime.datetime(2026, 1, 19), 'Martin Luther King Jr. Day: no classes'),
+    (datetime.datetime(2026, 2, 16), 'President\'s Day: no classes'),
+    (datetime.datetime(2026, 2, 17), 'Substitute Day: Monday Schedule'),
+    (datetime.datetime(2026, 3, 16), 'Spring Break: no classes'),
+    (datetime.datetime(2026, 3, 17), 'Spring Break: no classes'),
+    (datetime.datetime(2026, 3, 18), 'Spring Break: no classes'),
+    (datetime.datetime(2026, 3, 19), 'Spring Break: no classes'),
+    (datetime.datetime(2026, 3, 20), 'Spring Break: no classes'),
+    (datetime.datetime(2026, 4, 15), 'Ruhlman Conference: no classes'),
+    (datetime.datetime(2026, 4, 20), 'Patriot\'s Day: no classes'),
+    (datetime.datetime(2026, 4, 30), 'Substitute Day: Monday Schedule'),
+    (datetime.datetime(2025, 5, 1), 'Reading Period Begins.'),
+    (datetime.datetime(2025, 5, 5), 'Final Exam Period Begins.'),
 ]
 
 
-READING_PERIOD_START = datetime.datetime(2025, 12, 11)
+READING_PERIOD_START = datetime.datetime(2026, 5, 1)
 
 
 def is_date_special(current):
@@ -31,8 +35,8 @@ def is_date_special(current):
 
 
 def generate_yml_calendar():
-    course_start = datetime.datetime(2025, 9, 1)
-    course_end = datetime.datetime(2025, 12, 19)
+    course_start = datetime.datetime(2026, 1, 19)
+    course_end = datetime.datetime(2026, 5, 8)
 
     start = course_start - datetime.timedelta(days=course_start.weekday())
     end = course_end + datetime.timedelta(days=6 - course_end.weekday())
@@ -69,8 +73,6 @@ def generate_yml_calendar():
 
 
 def main():    
-    #generate_md_calendar()
-    #generate_csv_calendar()
     generate_yml_calendar()
 
     
